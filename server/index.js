c06396804f97c5c216a0a47c3a62baa9addb9454
Juan SB <juansb.dev@gmail.com>
2023-10-07 18:40:46 -0300

    server seted

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app = require('./src/app')
 const port = 0
-app.listen(port, ()=>{
-    console.log('listening on port'+ port)
+const server = app.listen(port, ()=>{
+    console.log('listening on port '+ server.address().port)
 })
 
