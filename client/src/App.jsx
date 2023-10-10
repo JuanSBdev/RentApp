8e7f75371945b47f8bb4fa09f523df0d966dfb8c
Juan SB <juansb.dev@gmail.com>
2023-10-10 16:11:17 -0300

    Landing Start Alpha

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MapView from './components/MapView'
+import Start from './components/Start/Start'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <MapView></MapView>
+      <Start/>
     </>
   )
 }
