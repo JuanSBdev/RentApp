7e423f68bfa7ceb46402193dcd38e52d12a864e0
Juan SB <juansb.dev@gmail.com>
2023-10-10 13:20:22 -0300

    Route getPlacebyId

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,12 @@
 const {Router} = require('express')
 
 const getPlaces = require('../handlers/getPlaces/getPlaces')
+const getPlaceById = require('../handlers/getPlaces/getPLaceByIdHandler')
+
 
 const router = Router();
 
 router.use('/start', getPlaces )
+router.use('/place/:id', getPlaceById )
 
 module.exports = router;
