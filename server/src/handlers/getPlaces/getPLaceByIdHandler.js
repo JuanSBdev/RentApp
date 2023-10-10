42e7dff12999d332674d8ea93eb40f02efe80ca2
Juan SB <juansb.dev@gmail.com>
2023-10-10 13:24:37 -0300

    FIX handler getPlaceById

--- server/src/handlers/getPlaces/getPLaceByIdHandler.js
+++ server/src/handlers/getPlaces/getPLaceByIdHandler.js
@@ -2,7 +2,7 @@ const getPlaceByIdController = require('../../controllers/getPlaces/getPLaceById
 const getPLaceByIdHandler = async (req, res)=>{
     let { id } = req.params
  try {
-    let result = getPlaceByIdController(id)
+    let result = await getPlaceByIdController(id)
     res.status(200).json(result)
  } catch (error) {
     res.status(400).json({ error: error.message });
