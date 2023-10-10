70e4bc002abbf30a2e97a0ca32d48196ba3bf925
Juan SB <juansb.dev@gmail.com>
2023-10-10 16:26:37 -0300

    setting enviroment

--- client/src/components/Start/Start.jsx
+++ client/src/components/Start/Start.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Styles from './Start.module.css'
+
 
 export default function Start() {
+    let imgInit = 'https://cdn.blessthisstuff.com/imagens/stuff/mountain-guest-house-switzerland.jpg'
+    
   return (
+    
     <div>
-        
+        <div className={Styles.nav}>
+            <p>Logo</p>
+            <input type="text" />
+            <p>Contact Us</p>
+            <p>About</p>
+        </div>
+        <div className={Styles.divImg}>
+
+            <img className={Styles.imgInit} src={imgInit}  alt="Cabaña entre montañas" />
+        </div>
     </div>
   )
 }
