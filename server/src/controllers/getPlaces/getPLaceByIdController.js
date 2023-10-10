370264ff5104437fc83b76417c85777b519ffd67
Juan SB <juansb.dev@gmail.com>
2023-10-10 13:15:35 -0300

    controller getPlaceById

--- server/src/controllers/getPlaces/getPLaceByIdController.js
+++ server/src/controllers/getPlaces/getPLaceByIdController.js
@@ -1,5 +1,7 @@
  const {Place} = require('../../db')
 const getPlaceByIdController = async (id)=>{
-    let placeFound = await Place.findByPk()
+    let placeFound = await Place.findByPk(id)
+    
+    return placeFound;
 }
 module.exports= getPlaceByIdController;
