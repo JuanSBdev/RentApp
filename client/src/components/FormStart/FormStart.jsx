0cc137793bb616e0a6509817f31ac2652a72d8b0
Juan SB <juansb.dev@gmail.com>
2023-10-11 13:53:19 -0300

    form

--- client/src/components/FormStart/FormStart.jsx
+++ client/src/components/FormStart/FormStart.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Styles from './FormStart.module.css'
 
 export default function FormStart() {
   return (
-    <div>
-        <form action="">
+    <div className={Styles.wrapper}>
+        <h2>Buscar alojamientos</h2>
+        <form action="" >
             <input  name='place' type="text" />
             <input  name='date' type="date" />
             <input name='guests'  type="number" />
