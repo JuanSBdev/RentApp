eadffa12f7a1a02635373e68e7fae052f360648a
Juan SB <juansb.dev@gmail.com>
2023-10-12 17:29:44 -0300

    FT - getplacebylocation handler & controller & route

--- server/src/controllers/getPlaces/getPlaceByLocationController.js
+++ server/src/controllers/getPlaces/getPlaceByLocationController.js
@@ -1,15 +1,16 @@
 const {Place} = require('../../db')
 const { Op } = require('sequelize')
 const getPlaceByLocationController = async (location)=>{
- const placeFound = Place.findAll({
+ const placeFound = await  Place.findAll({
     where:{
-      location:{
+      location:
+      {
          [Op.iLike]: `%${location}%`
       }
  },
 }
  )
-
+console.log(location)
  return placeFound;
 }
 module.exports = getPlaceByLocationController;
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,13 +2,13 @@ const {Router} = require('express')
 
 const getPlaces = require('../handlers/getPlaces/getPlaces')
 const getPlaceById = require('../handlers/getPlaces/getPLaceByIdHandler');
-const getPlaceByLocationHandler = require('../handlers/getPlaces/getPlaceByPlaceHandler');
+const getPlaceByLocationHandler = require('../handlers/getPlaces/getPlaceByLocationHandler');
 
 
 const router = Router();
 
-router.use('/', getPlaces )
+router.use('/start', getPlaces )
 router.use('/place/:id', getPlaceById )
-router.use('/place/:location', getPlaceByLocationHandler )
+router.use('/place/in/:location', getPlaceByLocationHandler )
 
 module.exports = router;
