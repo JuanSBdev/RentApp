582dad4c74f072eec62d441ab04ada3d3df19484
Juan SB <juansb.dev@gmail.com>
2023-10-19 22:26:58 -0300

    NoPlace cards

--- client/src/components/Inicio/NoPlace.jsx
+++ client/src/components/Inicio/NoPlace.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../../../dist/output.css'; // Ajusta la ruta según la ubicación de tu archivo CSS
 import Capilla from './CapillaDelMonte/Capilla';
+import Styles from './NoPlace.module.css'
 
 
 export default function NoPlace() {
   return (
-    <div>
+    <div className={`${Styles.wrapper} flex flex-row justify-around -mt-40 ` }>
+      <Capilla/>
       <Capilla/>
     </div>
   )
