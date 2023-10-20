4268539763e6bbdef1d4d190be667d4943be1f60
Juan SB <juansb.dev@gmail.com>
2023-10-19 22:12:37 -0300

    Capilla card

--- client/src/components/Inicio/NoPlace.jsx
+++ client/src/components/Inicio/NoPlace.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../../../dist/output.css'; // Ajusta la ruta según la ubicación de tu archivo CSS
+import Capilla from './CapillaDelMonte/Capilla';
 
 
 export default function NoPlace() {
   return (
     <div>
-
+      <Capilla/>
     </div>
   )
 }
