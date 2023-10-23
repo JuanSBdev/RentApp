f7db48e771f8e8ce5310aa56fe342c0cdd12e117
Juan SB <juansb.dev@gmail.com>
2023-10-22 23:40:11 -0300

    FEAT - Route Guests

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,11 +3,13 @@ const {Router} = require('express')
 const getPlaces = require('../handlers/getPlaces/getPlaces')
 const getPlaceById = require('../handlers/getPlaces/getPLaceByIdHandler');
 const getPlaceByLocationHandler = require('../handlers/getPlaces/getPlaceByLocationHandler');
+const getPlaceByGuestsHandler = require('../handlers/getPlaces/getPlaceByGuestsHandler');
 
 
 const router = Router();
 router.use('/start', getPlaces )
 router.use('/place/in/:location', getPlaceByLocationHandler )
+router.use('/place/for/:guests', getPlaceByGuestsHandler )
 router.use('/place/:id', getPlaceById )
 
 module.exports = router;
