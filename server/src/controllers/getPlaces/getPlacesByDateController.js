24791848557b9059cbb1663a73d192da533743d7
Juan SB <juansb.dev@gmail.com>
2023-10-24 22:03:25 -0300

    FIX getByDate Handler & controller

--- server/src/controllers/getPlaces/getPlacesByDateController.js
+++ server/src/controllers/getPlaces/getPlacesByDateController.js
@@ -4,13 +4,18 @@ const {Op} = require('sequelize')
 const getPlacesByDateController = async (dateInit, dateFinish)=>{
     
     // const excludedDates = [dateInit, dateFinish];
-    let result = await Place.findAll({
+    let overlap = await Place.findAll({
         where:{
             unavailable_dates:{
                 [Op.overlap]:[dateInit, dateFinish]
             }
         }
     })
-    return result;
+    let all = await Place.findAll()
+    let notOverlap = all.filter(place => {
+        return !overlap.some(o => o.id === place.id)
+    })
+
+    return notOverlap;
 }
 module.exports= getPlacesByDateController;
