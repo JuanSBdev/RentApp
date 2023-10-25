a87c2b540422f03d283e3abf9d0db92b8f8eed6a
Juan SB <juansb.dev@gmail.com>
2023-10-24 22:12:30 -0300

    FEATURE -geytByDate REDUX action

--- client/src/redux/action_types.js
+++ client/src/redux/action_types.js
@@ -1,2 +1,3 @@
 export const GET_NAME = "GET_BY_NAME"
 export const GET_GUESTS = "GET_BY_GUESTS"
+export const GET_DATE = "GET_BY_DATE"
--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import  {GET_GUESTS, GET_NAME} from './action_types'
+import  {GET_DATE, GET_GUESTS, GET_NAME} from './action_types'
  
 export const getPlaceByName = (placeName) =>{
     return async(dispatch) =>{
@@ -32,6 +32,23 @@ export const getPlaceByGuest = (guestsQ)=>{
             })
         } catch (error) {
             console.log(error + "redux error")
+        }
+    }
+}
+
+const getPlaceByDate = async (dateInit, dateFinish)=>{
+    return async (dispatch)=>{
+        let endpoint =  `http://localhost:63076/place/dates/?dateInit=${dateInit}&dateFinish=${dateFinish}`
+        try {
+            let response = await axios(endpoint);
+            let data = response.data;
+            dispatch({
+                type: GET_DATE,
+                payload: data
+            })
+        } catch (error) {
+            console.log("redux error get_date" + error )
+
         }
     }
 }
