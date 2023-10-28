0f91e1b41b511d6be00097c785ac5161f2bd7d64
Juan SB <juansb.dev@gmail.com>
2023-10-28 18:47:56 -0300

    buscar card capilla dispatch

--- client/src/components/Inicio/CapillaDelMonte/Capilla.jsx
+++ client/src/components/Inicio/CapillaDelMonte/Capilla.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import '../../../../dist/output.css'; // Ajusta la ruta según la ubicación de tu archivo CSS
 import Styles from './Capilla.module.css'
+import { useDispatch } from 'react-redux';
+import { getPlaceByName } from '../../../redux/actions';
+
 
 
 export default function Capilla() {
+
+
+    let dispatch = useDispatch()
+    let buscarEnCapilla = ()=>{
+        dispatch(getPlaceByName('capilla'))
+    }
   return (
     <div>
 
@@ -17,12 +26,12 @@ export default function Capilla() {
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Temporada Capilla Del Monte 2023</h5>
         </a>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Encontrá tu alojamiento al mejor precio en Capilla Del Monte para esta temporada 2023</p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <button onClick={buscarEnCapilla}  class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Buscar
              <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
-        </a>
+        </button>
     </div>
 </div>
 
