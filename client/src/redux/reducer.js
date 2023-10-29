cc79edc85dc21597fea5ef73fd50c69a2894367c
Juan SB <juansb.dev@gmail.com>
2023-10-28 21:00:58 -0300

    FT- REDUX REDUCER

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,10 +1,16 @@
-import { GET_DATE, GET_GUESTS, GET_NAME } from "./action_types"
+import { GET_DATE, GET_FORM, GET_GUESTS, GET_NAME } from "./action_types"
 const initialState = {
     place: []
 }
 
  const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case GET_FORM:
+            return{
+                ...state,
+                place: action.payload
+
+            }
         case GET_NAME:
             return {
                 ...state,
