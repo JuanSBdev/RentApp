85c689ca110434e7a2b513284873725faa3433b2
Juan SB <juansb.dev@gmail.com>
2023-10-31 17:25:57 -0300

    FEAT -  User MODEL

---
+++ server/src/models/user.js
@@ -0,0 +1,27 @@
+const {DataTypes, sequelize } = require('sequelize')
+
+module.exports = (sequelize) =>{
+    sequelize.define( 'User',{
+        id:{
+            type:DataTypes.UUID,
+            primaryKey:true,
+            defaultValue:DataTypes.UUIDV4,
+        },
+        mail:{
+            type:DataTypes.STRING,
+            allowNull:false,
+            unique:true,
+            validate:{
+                notEmpty:{
+                    msg:`Place's name couldn't be null`
+                },
+            },
+        },
+        isAdmin:{
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+        }
+    }
+    )
+}
+
