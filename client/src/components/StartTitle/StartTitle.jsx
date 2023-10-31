f6eb28e60f5eba62f6a728d26e48a758f5f3f552
Juan SB <juansb.dev@gmail.com>
2023-10-31 00:21:37 -0300

    FT - LANG  text init

--- client/src/components/StartTitle/StartTitle.jsx
+++ client/src/components/StartTitle/StartTitle.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Styles from './StartTitle.module.css'
+import { useSelector } from 'react-redux'
 export default function StartTitle() {
+
+  let lang = useSelector(state=> state.language )
+
   return (
     <div className={`${Styles.wrapper} flex justify-center w-100 mx-2 mt-10`}>
+      {lang === 'es' ? (
         <h1 className='flex justify-start '> Encontrá tu lugar en Córdoba, en Kirá</h1>
+      ):(
+        <h1 className='flex justify-start '> Discover your place in Córdoba, at Kirá</h1>
+
+      )  }
     </div>
   )
 }
