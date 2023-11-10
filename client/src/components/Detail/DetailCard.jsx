cd81e2c5f9e1e26bfb983f0ce31f32ac8a87d91b
Juan SB <juansb.dev@gmail.com>
2023-11-09 23:27:59 -0300

    ft - DETAIL COMPONENT

---
+++ client/src/components/Detail/DetailCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import styles from './DetailCard.module.css'
+export default function DetailCard() {
+  return (
+    <div>DetailCard</div>
+  )
+}
--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -15,18 +15,21 @@ export default function Detail() {
   let places = useSelector(state => state.detail)
   return (
     <div className={styles.wrapper} >
+      <div className={styles.placeCard}>
+
     { places.map((place, index) => (
-            <Places key={index}
-            id={place.id}
-            max_occupancy_per_room={place.max_occupancy_per_room}
-            type={place.type}
-            name={place.name}
-            location={place.location}
-            price_per_night={place.price_per_night}
-            rating={place.rating}
-            
-            />
-        ))}
+      <Places key={index}
+      id={place.id}
+      max_occupancy_per_room={place.max_occupancy_per_room}
+      type={place.type}
+      name={place.name}
+      location={place.location}
+      price_per_night={place.price_per_night}
+      rating={place.rating}
+      
+      />
+      ))}
+      </div>
 
     </div>
   )
