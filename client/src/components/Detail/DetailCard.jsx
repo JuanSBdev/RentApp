2498e785528150909e174766bcd5a5f6e29feb0e
Juan SB <juansb.dev@gmail.com>
2023-11-11 18:50:01 -0300

    green

--- client/src/components/Detail/DetailCard.jsx
+++ client/src/components/Detail/DetailCard.jsx
@@ -3,7 +3,7 @@ import styles from './DetailCard.module.css'
 import SliderTss from './Slider/SliderTws'
 export default function DetailCard() {
   return (
-    <div className='w-9/12 pt-5   '>
+    <div className='w-9/12 pt-4 '>
         <SliderTss/>
     </div>
   )
