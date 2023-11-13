53a1cdb1bcd557eec793af0cae1aa20ed1d19f92
Juan SB <juansb.dev@gmail.com>
2023-11-12 23:10:40 -0300

    FT - Card Info

--- client/src/components/Detail/Prices/PlaceData.jsx
+++ client/src/components/Detail/Prices/PlaceData.jsx
@@ -5,23 +5,44 @@ import {
     Typography,
     Button,
   } from "@material-tailwind/react";
-   
+  import { useSelector } from 'react-redux'
+  
   export default function PlaceData() {
+    let place = useSelector(state => state.detail[0])
     return (
       <Card className="mt-6 w-96">
+       
         <CardBody>
           <Typography variant="h5" color="blue-gray" className="mb-2">
             Info
           </Typography>
-          <Typography>
+          <Typography variant="p">
             The place is close to Barceloneta Beach and bus stop just 2 min by
             walk and near to &quot;Naviglio&quot; where you can enjoy the main
             night life in Barcelona.
           </Typography>
+          {place && <Typography variant="p">
+            Price per night {place.price_per_night}$
+          </Typography>}
+          <Typography variant="p">
+            Services: Wi-fi, garage, breackfast, bedrooms, private bath, room service
+          </Typography>
         </CardBody>
-        <CardFooter className="pt-0">
+        {/* <div className=" flex mx-4 mb-4 ">
+        <p className="px-2 text-black">Services:</p>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z" />
+</svg><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z" />
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z" />
+</svg>
+        </div> */}
+        {/* <CardFooter className="pt-0">
           <Button>Read More</Button>
-        </CardFooter>
+        </CardFooter> */}
+        
       </Card>
     );
   }
--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -8,20 +8,17 @@ import SliderTss from '../../components/Detail/Slider/SliderTws'
 import DetailCard from '../../components/Detail/DetailCard'
 
 export default function Detail() {
-let [loading, setLoading] = useState(true)
+
 
   let dispatch = useDispatch()
   let {id} = useParams()
   useEffect(()=>{
     dispatch(getDetail(id))
-    setLoading(false)
-    console.log(loading)
   },[id])
 
   let places = useSelector(state => state.detail[0])
   return (
-    <div className={`${styles.wrapper} flex  justify-center align-center `} >
-              {!loading && places && <h1 className=' ml-5 py-5 text-white'>{places.name}</h1> }
+    <div className={`${styles.wrapper} flex justify-center align-center `} >
 
       
       <DetailCard/>
