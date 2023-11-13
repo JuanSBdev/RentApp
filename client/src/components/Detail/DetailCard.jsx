9846e432484035003de669d5fb96c64206817d2d
Juan SB <juansb.dev@gmail.com>
2023-11-12 21:30:57 -0300

    Ft - Detail place

--- client/src/components/Detail/DetailCard.jsx
+++ client/src/components/Detail/DetailCard.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import styles from './DetailCard.module.css'
+import { useSelector } from 'react-redux'
 import SliderTss from './Slider/SliderTws'
 export default function DetailCard() {
+  let place = useSelector(state => state.detail[0])
   return (
-    <div className=' pt-4'>
-          <h2>Tu alojamiento</h2>
-          <div className="flex justify-center">
-
-            <SliderTss/>
+    <div className=' pt-4 flex items-center flex-col'>
+          <div className=" self-start">
+              <h2 className='ml-5 py-5 text-white'>{place.name}</h2>
           </div>
+            <SliderTss/>
         
     </div>
   )
