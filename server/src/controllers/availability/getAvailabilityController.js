915d242e82f945d28de3fd657ff383033f627091
Juan SB <juansb.dev@gmail.com>
2023-11-15 00:14:35 -0300

    FT getAvailabilityController

--- server/src/controllers/availability/getAvailabilityController.js
+++ server/src/controllers/availability/getAvailabilityController.js
@@ -3,12 +3,22 @@ let {Place} = require('../../db')
 let getAvailabilityController = async (placeId, dateInit, dateEnd, guests)=>{
     let result = await Place.findAll({
         where:{
-            
-            unavailable_dates:{
-                [Op.overlap]:[dateInit, dateEnd]
+            id: placeId,
+            number_of_rooms: {
+                [Op.gt]: 1
+            },
+            max_occupancy_per_room:{
+                [Op.gte]: Number(guests)
+            },
+            [Op.not]:{
+                unavailable_dates:{
+                    [Op.overlap]:[dateInit, dateEnd]
+                }
+
             }
         }
     })
+    console.log(dateInit, dateEnd)
     return result
 }
 module.exports = getAvailabilityController;
--- server/src/handlers/abiability/getAvailabilityHandler.js
+++ server/src/handlers/abiability/getAvailabilityHandler.js
@@ -4,13 +4,13 @@ const getPlaceByIdController = require("../../controllers/getPlaces/getPlaceById
 const getAvailabilityController = require("../../controllers/availability/getAvailabilityController")
 
 let getAvailabilityHandler = async (req, res)=>{
-    let {id, dateInit, dateFinish, guests, location} = req.body
+    let {id, dateInit, dateEnd, guests, location} = req.body
     try {
         // let resultDate = getPlacesByDateController(dateInit, dateFinish)
         // let resultGuests = getPlacesByGuestsController(location, guests)
         // let resultId = getPlaceByIdController(id)
 
-        let result = await getAvailabilityController(id, dateInit, dateFinish, guests)
+        let result = await getAvailabilityController(id, dateInit, dateEnd, guests)
          res.status(200).json(result)
     } catch (error) {
         res.status(400).json(error)
