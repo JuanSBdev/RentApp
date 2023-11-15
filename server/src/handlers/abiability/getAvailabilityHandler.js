f0db295877dafa6ec5a5359d29644531f330ee47
Juan SB <juansb.dev@gmail.com>
2023-11-15 19:09:13 -0300

    Cleaning

--- server/src/handlers/abiability/getAvailabilityHandler.js
+++ server/src/handlers/abiability/getAvailabilityHandler.js
@@ -1,6 +1,3 @@
-let getPlacesByDateController = require("../../controllers/getPlaces/getPlacesByDateController")
-const getPlacesByGuestsController = require("../../controllers/getPlaces/getPlacesByGuestsController")
-const getPlaceByIdController = require("../../controllers/getPlaces/getPlaceByIdController")
 const getAvailabilityController = require("../../controllers/availability/getAvailabilityController")
 
 let getAvailabilityHandler = async (req, res)=>{
