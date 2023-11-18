8e25ee237d8337eba429d2b3b57f6e5adc4e4bfa
Juan SB <juansb.dev@gmail.com>
2023-11-17 22:21:33 -0300

    FT - Availability data FRONT

--- client/src/components/Detail/price/Options.jsx
+++ client/src/components/Detail/price/Options.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export default function Options() {
-  return (
-    <div >
-        <p>Habitacion Privada</p>
-        <p>2 personas</p>
-        <p>desde 10/01/2024</p>
-        <p>Hasta 14/01/2024</p>
-        <p>Total: 300U$D</p>
+  let place = useSelector(state => state.detail[0])
+  let price = useSelector(state => state.availability)
+  return (<div className="">
+    {price && place &&
+    <div>
+    <p>Room: {place.type}</p>
+    <p>{place.max_occupancy_per_room}</p>
+    <p>desde 10/01/2024</p>
+    <p>Hasta 14/01/2024</p>
+    <p>Total: 300U$D</p>
     </div>
+  }
+  </div>
   )
 }
