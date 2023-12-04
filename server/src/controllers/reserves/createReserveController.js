900bb8c6f6a201c98d624e755385a96fb9c55be0
Juan SB <juansb.dev@gmail.com>
2023-12-04 19:02:20 -0300

    reserve  HANDLER & CONTROLLER - placeId , User, Total Price

--- server/src/controllers/reserves/createReserveController.js
+++ server/src/controllers/reserves/createReserveController.js
@@ -1,13 +1,15 @@
 const { User, Reserve, Place } = require('../../db');
+const getPrice = require('../../handlers/availability/getPrice');
 const createReserveController = async ( dateInit, dateEnd, userId, placeId )=>{
     // console.log(dateInit, dateEnd, userId + 'datos controller ')
     const userFound = await User.findByPk(userId)
     const placeFound = await Place.findByPk(placeId)
-    let newReserve = await Reserve.create({dateInit, dateEnd})
+    let total = await getPrice(placeFound, dateInit, dateEnd )
     
+    let newReserve = await Reserve.create({dateInit, dateEnd, total})
     await newReserve.setUser(userFound);
     await newReserve.setPlace(placeFound);
 
     return newReserve;
 }
-module.exports = createReserveController;
+module.exports = createReserveController;8
