8942e6df8637e8fb8aa08c2cddc8c2ef2c1cc0dc
Juan SB <juansb.dev@gmail.com>
2023-12-04 19:18:28 -0300

    array unavailñable dates

---
+++ server/src/controllers/reserves/getDatesArray.js
@@ -0,0 +1,13 @@
+function getDatesArray(startDate, endDate) {
+    const datesArray = [];
+    let currentDate = new Date(startDate);
+
+    while (currentDate <= new Date(endDate)) {
+        datesArray.push(currentDate.toISOString().split('T')[0]);
+        currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    return datesArray;
+}
+
+module.exports = createReserveController;
