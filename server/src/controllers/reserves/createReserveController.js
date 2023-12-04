67e77b8ed7de4391576252b6b1644d6589f8f71b
Juan SB <juansb.dev@gmail.com>
2023-12-03 23:50:11 -0300

    SET PLACE reserve controller

--- server/src/controllers/reserves/createReserveController.js
+++ server/src/controllers/reserves/createReserveController.js
@@ -1,8 +1,8 @@
-const { User, Reserve } = require('../../db');
-const createReserveController = async ( dateInit, dateEnd, userId )=>{
-    console.log(dateInit, dateEnd, userId + 'datos controller ')
+const { User, Reserve, Place } = require('../../db');
+const createReserveController = async ( dateInit, dateEnd, userId, placeId )=>{
+    // console.log(dateInit, dateEnd, userId + 'datos controller ')
     const userFound = await User.findByPk(userId)
-    console.log(userFound + 'userfound')
+    const placeFound = await Place.findByPk(placeId)
     let newReserve = await Reserve.create({dateInit, dateEnd})
     
     await newReserve.setUser(userFound);
--- server/src/handlers/reserves/createReserveHandler.js
+++ server/src/handlers/reserves/createReserveHandler.js
@@ -1,9 +1,9 @@
 const createReserveController = require("../../controllers/reserves/createReserveController");
 
 const createReserveHandler = async (req, res)=>{
-    let { dateInit, dateEnd, userId } =req.body
+    let { dateInit, dateEnd, userId, placeId } =req.body
     try {
-        const result = await createReserveController(dateInit, dateEnd, userId)
+        const result = await createReserveController(dateInit, dateEnd, userId, placeId)
         res.status(200).json(result)
     } catch (error) {
         res.status(400).json({error:error.message});
