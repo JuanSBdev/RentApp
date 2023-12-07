a1a16adb77e06aea7bd77ca81695e1a59615e172
Juan SB <juansb.dev@gmail.com>
2023-12-07 00:59:01 -0300

    FT -style availability

--- client/src/components/Detail/price/Options.jsx
+++ client/src/components/Detail/price/Options.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import Style from './Options.module.css'
 
 export default function Options() {
   let availability = useSelector(state => state.availability)
-  return (<div className="">
+  return (
+    <div className={Style.container}>
     {availability && availability.price &&
-    <div>
-    <p>Room: {availability.place.type}</p>
-    <p>Max Guests {availability.place.max_occupancy_per_room}</p>
-    <p>desde {availability.dateInit}</p>
-    <p>Hasta {availability.dateEnd}</p>
-    <p>Total: {availability.price}</p>
+      <div className={Style.wrapper}>
+        <div className={Style.data}>
+          <p>Room: {availability.place.type}</p>
+          <p>Max Guests {availability.place.max_occupancy_per_room}</p>
+          <p>desde {availability.dateInit}</p>
+          <p>Hasta {availability.dateEnd}</p>
+          <p>Total: {availability.price}</p>
+        </div>
+        <button className='btn m-4'> Reserve </button>
+      </div>
+    }
     </div>
-  }
-  </div>
   )
 }
