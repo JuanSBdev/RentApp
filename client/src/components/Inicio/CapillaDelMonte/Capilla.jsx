903d5e91ec2a0278992383d7a23b258f58493a9b
Juan SB <juansb.dev@gmail.com>
2023-12-10 18:44:50 -0300

    Cards Inicio BTN styles

--- client/src/components/Inicio/CapillaDelMonte/Capilla.jsx
+++ client/src/components/Inicio/CapillaDelMonte/Capilla.jsx
@@ -28,7 +28,7 @@ export default function Capilla() {
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Temporada Capilla Del Monte 2024</h5>
         </a>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Encontrá tu alojamiento al mejor precio en Capilla Del Monte, Córdoba para esta temporada 2024</p>
-        <button onClick={buscarEnCapilla}  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <button onClick={buscarEnCapilla}  className="flex items-center justify-center h-12 px-6 w-64 bg-[#259073] mt-8 rounded font-bold text-sm text-white  hover:bg-[#7fda89] mx-2">
             Buscar
              <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -48,7 +48,7 @@ export default function Capilla() {
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> Capilla Del Monte season 2024</h5>
                     </a>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Explore the best accommodation rates in Capilla Del Monte, Córdoba, for the upcoming 2024 season.</p>
-                    <button onClick={buscarEnCapilla}  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <button onClick={buscarEnCapilla}  className="flex items-center justify-center h-12 px-6 w-64 bg-[#259073] mt-8 rounded font-bold text-sm text-white  hover:bg-[#7fda89] mx-2">
                         Search here
                          <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
--- client/src/components/Inicio/CarlosPaz+/Carlos.jsx
+++ client/src/components/Inicio/CarlosPaz+/Carlos.jsx
@@ -26,7 +26,7 @@ export default function Carlos() {
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Temporada San Marcos Sierras  2024</h5>
                 </a>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Encontrá tu alojamiento al mejor precio en San Marcos Sierras, Córdoba, para esta temporada 2023</p>
-                <a onClick={buscarCarlos} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <a onClick={buscarCarlos} className="flex items-center justify-center h-12 px-6 w-64 bg-[#259073] mt-8 rounded font-bold text-sm text-white  hover:bg-[#7fda89] mx-2">
                     Buscar
                      <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -44,7 +44,7 @@ export default function Carlos() {
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">San Marcos Sierras season  2024</h5>
         </a>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Discover the most budget-friendly accommodations in San Marcos Sierras, Córdoba, for the 2024 season.</p>
-        <a onClick={buscarCarlos} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <a onClick={buscarCarlos} className="flex items-center justify-center h-12 px-6 w-64 bg-[#259073] mt-8 rounded font-bold text-sm text-white  hover:bg-[#7fda89] mx-2">
             search here
              <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
