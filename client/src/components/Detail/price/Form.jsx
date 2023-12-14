c4ffc8c8b95cc8f5edebec278879943d844ac1e3
Juan SB <juansb.dev@gmail.com>
2023-12-14 20:38:18 -0300

    FIX formA options ENGLISH

--- client/src/components/Detail/price/Form.jsx
+++ client/src/components/Detail/price/Form.jsx
@@ -85,43 +85,65 @@ return (
            </div>
              <button onClick={submitForm} className="flex items-center justify-center h-12 px-6 w-64            bg-[#259073] mt-8 rounded font-bold text-sm text-blue-100            hover:bg-[#7fda89] mx-2">Ver precios</button>
    
- </form>
+       </form>
   ):(
     <form  className=" border border-[#259073] flex flex-col items-center bg-[#fcffee] rounded shadow-lg p-5 mt-0  lg:flex-row " action="">
-           <div >
-
-             <label className="font-semibold text-xs" htmlFor="usernameField">City</label>
-             <select onChange={handleForm} name='city' id="ciudades" className="flex items-center h-12 px-4 w-34  bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2">
-                   <option value="capilla">Capilla del Monte</option>
-                   <option value="carlos">Carlos Paz</option>
-                   <option value="falda">La Falda</option>
-             </select>
-             
-           </div>
+           
            <div className="m-1">
 
-             <label className="font-semibold text-xs mt-3 m-0 lg:ml-5" htmlFor="passwordField ">From</           label>
+             <label className="font-semibold text-xs mt-3 m-0 lg:ml-5" htmlFor="passwordField ">Desde</           label>
              <input onChange={handleForm} name='dateFrom' type="date" id="dateInput" className="flex items-center h-12 px-4 w-34 bg-gray-200 ml-0 mt-2              rounded focus:outline-none focus:ring-2 lg:ml-5"/>
 
            </div>
            <div className="m-1">
 
-             <label className="font-semibold text-xs mt-3 m-0 lg:ml-5"  htmlFor="passwordField">To</label>
-             <input onChange={handleForm}
-            //   type={inputType} onClick={()=>setInputType('date')}
-            //  onBlur={()=>setInputType('text')}
-             type='date'
-             placeholder='dd-mm-yyyy' name='dateTo' id="dateInput" className="flex items-center h-12 px-4 w-34 bg-gray-200 ml-0 mt-2  rounded focus:outline-none focus:ring-2 lg:ml-5"/>
+             <label className="font-semibold text-xs mt-3 m-0 lg:ml-5"  htmlFor="passwordField">Hasta</           label>
+             <input onChange={handleForm} type="date" name='dateTo' id="dateInput" className="flex items-center h-12 px-4 w-34 bg-gray-200 ml-0 mt-2  rounded focus:outline-none focus:ring-2 lg:ml-5"/>
 
            </div>
            <div className="m-1">
 
-             <label className="font-semibold text-xs mt-3"             htmlFor="passwordField">Guests</label>
+             <label className="font-semibold text-xs mt-3"             htmlFor="passwordField">Personas</label>
              <input onChange={handleForm} name='guests' className="flex items-center h-12 px-4 w-20 bg-gray-200 mt-2             rounded focus:outline-none focus:ring-2"type="number" min="0" placeholder='x1'/>
            </div>
-             <button onClick={submitForm} className="flex items-center justify-center h-12 px-6 w-64            bg-[#259073] mt-8 rounded font-bold text-sm text-blue-100            hover:bg-[#7fda89] mx-2">Check Prices</button>
+             <button onClick={submitForm} className="flex items-center justify-center h-12 px-6 w-64            bg-[#259073] mt-8 rounded font-bold text-sm text-blue-100            hover:bg-[#7fda89] mx-2">Ver precios</button>
    
-      </form>
+       </form>
+    // <form  className=" border border-[#259073] flex flex-col items-center bg-[#fcffee] rounded shadow-lg p-5 mt-0  lg:flex-row " action="">
+    //        <div >
+
+    //          <label className="font-semibold text-xs" htmlFor="usernameField">City</label>
+    //          <select onChange={handleForm} name='city' id="ciudades" className="flex items-center h-12 px-4 w-34  bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2">
+    //                <option value="capilla">Capilla del Monte</option>
+    //                <option value="carlos">Carlos Paz</option>
+    //                <option value="falda">La Falda</option>
+    //          </select>
+             
+    //        </div>
+    //        <div className="m-1">
+
+    //          <label className="font-semibold text-xs mt-3 m-0 lg:ml-5" htmlFor="passwordField ">From</           label>
+    //          <input onChange={handleForm} name='dateFrom' type="date" id="dateInput" className="flex items-center h-12 px-4 w-34 bg-gray-200 ml-0 mt-2              rounded focus:outline-none focus:ring-2 lg:ml-5"/>
+
+    //        </div>
+    //        <div className="m-1">
+
+    //          <label className="font-semibold text-xs mt-3 m-0 lg:ml-5"  htmlFor="passwordField">To</label>
+    //          <input onChange={handleForm}
+    //         //   type={inputType} onClick={()=>setInputType('date')}
+    //         //  onBlur={()=>setInputType('text')}
+    //          type='date'
+    //          placeholder='dd-mm-yyyy' name='dateTo' id="dateInput" className="flex items-center h-12 px-4 w-34 bg-gray-200 ml-0 mt-2  rounded focus:outline-none focus:ring-2 lg:ml-5"/>
+
+    //        </div>
+    //        <div className="m-1">
+
+    //          <label className="font-semibold text-xs mt-3"             htmlFor="passwordField">Guests</label>
+    //          <input onChange={handleForm} name='guests' className="flex items-center h-12 px-4 w-20 bg-gray-200 mt-2             rounded focus:outline-none focus:ring-2"type="number" min="0" placeholder='x1'/>
+    //        </div>
+    //          <button onClick={submitForm} className="flex items-center justify-center h-12 px-6 w-64            bg-[#259073] mt-8 rounded font-bold text-sm text-blue-100            hover:bg-[#7fda89] mx-2">Check Prices</button>
+   
+    //   </form>
     ) 
 }
     </div>
