c5c7870df8d32cb40a4d0c6ebade5399b6f20b37
Juan SB <juansb.dev@gmail.com>
2023-12-18 15:30:38 -0300

    FT Options Lang changes

--- client/src/components/Detail/price/Options.jsx
+++ client/src/components/Detail/price/Options.jsx
@@ -36,13 +36,20 @@ export default function Options() {
   return (
     <div className={`${Style.container} bg-[#fcffee] m-5`}>
       <div className={`${Style.wrapper} `}> 
-        <div className={Style.data}>
-          <p>Room: {availability.place.type}</p>
-          <p>Max Guests {availability.place.max_occupancy_per_room}</p>
+       { lang === 'es' ? (<div className={Style.data}>
+          <p>Habitacion: {availability.place.type}</p>
+          <p>Para {availability.place.max_occupancy_per_room}</p>
           <p>desde {availability.dateInit}</p>
           <p>Hasta {availability.dateEnd}</p>
-          <p>Total: {availability.price}</p>
-        </div>
+          <p>Precio: {availability.price}U$D</p>
+        </div>):
+       ( <div className={Style.data}>
+          <p>Room: {availability.place.type}</p>
+          <p>Max Guests {availability.place.max_occupancy_per_room}</p>
+          <p>From {availability.dateInit}</p>
+          <p>To {availability.dateEnd}</p>
+          <p>Price: {availability.price}U$D</p>
+        </div>)}
         { !reserved ? (
           <button className='flex items-center justify-center h-12 px-6 w-64            bg-[#259073] rounded font-bold text-sm text-blue-100 hover:bg-[#7fda89] mt-3.5' onClick={()=> makeReserve()}> Reserve </button>
           ):(
