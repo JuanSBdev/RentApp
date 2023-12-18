542e71793d5c17832d9c38d2378fa6f44b75dd6c
Juan SB <juansb.dev@gmail.com>
2023-12-18 17:40:40 -0300

    reserves Data FEATure

--- client/src/views/Reserves/ReservesView.jsx
+++ client/src/views/Reserves/ReservesView.jsx
@@ -13,21 +13,37 @@ export default function Reserves(){
 
     let lang = useSelector(state=> state.language)
     let reservesRdx = useSelector(state=> state.reserves)
+    let placeRdx = useSelector(state=> state.place)
  return(
     <div className="">
         {lang === 'es' ? (
             <div className="">
 
                     <h1 className="text-white m-4">Mis reservas</h1>
-                    {reservesRdx.map(r =>( <p className="text-white">{r.id}</p> ))}
+                    {reservesRdx.map(r =>( 
+                        <div className="">
+                            <p className="text-white"> { placeRdx.find((p) => p.id === r.placeId)?.name  }</p>
+                            <p className="text-white"> Estado {r.status}</p>
+                            <p className="text-white"> desde {r.dateInit}</p>
+                            <p className="text-white"> hasta {r.dateEnd}</p>
+                            <p className="text-white"> precio {r.total}$</p>
+                        </div>
+
+                     ))}
             </div>
                     
 
         ):(
         <div className="">
-
-<h1 className="text-white m-4">Reserves</h1>
-{reservesRdx.map(r =>( <p className="text-white">{r.id}</p> ))}
+            <h1 className="text-white m-4">Reserves</h1>
+            {reservesRdx.map(r =>(
+                <div className="">
+                            <p className="text-white"> Status {r.status}</p>
+                            <p className="text-white"> from {r.dateInit}</p>
+                            <p className="text-white"> to {r.dateEnd}</p>
+                            <p className="text-white"> price {r.total}$</p>
+                        </div>
+                                     ))}
         </div>
         )}
     </div>
