0436044be5f75e5a18bdff71e0d0da5c050f4ef8
Juan SB <juansb.dev@gmail.com>
2023-12-18 17:41:46 -0300

    reserves Data FEATure II

--- client/src/views/Reserves/ReservesView.jsx
+++ client/src/views/Reserves/ReservesView.jsx
@@ -38,6 +38,7 @@ export default function Reserves(){
             <h1 className="text-white m-4">Reserves</h1>
             {reservesRdx.map(r =>(
                 <div className="">
+                            <p className="text-white"> { placeRdx.find((p) => p.id === r.placeId)?.name  }</p>
                             <p className="text-white"> Status {r.status}</p>
                             <p className="text-white"> from {r.dateInit}</p>
                             <p className="text-white"> to {r.dateEnd}</p>
