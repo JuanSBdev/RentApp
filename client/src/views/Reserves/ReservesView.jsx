722d52c9b369a231b189412b1c516fe78696984c
Juan SB <juansb.dev@gmail.com>
2023-12-23 03:21:06 -0300

    Ft reserves ENG lang

--- client/src/views/Reserves/ReservesView.jsx
+++ client/src/views/Reserves/ReservesView.jsx
@@ -54,18 +54,38 @@ export default function Reserves(){
             </div>
                     
         ):(
-        <div className="">
-            <h1 className="text-white m-4">Reserves</h1>
-            {reservesRdx.map(r =>(
-                <div className="">
-                            <p className="text-white"> { placeRdx.find((p) => p.id === r.placeId)?.name  }</p>
-                            <p className="text-white"> Status {r.status}</p>
-                            <p className="text-white"> from {r.dateInit}</p>
-                            <p className="text-white"> to {r.dateEnd}</p>
-                            <p className="text-white"> price {r.total}$</p>
+            <div className={Styles.wrapperEs}>
+
+            {reservesRdx.map(r =>( 
+                <div className={Styles.reserves}>
+                    
+                        <div className={Styles.placeName}>
+                            <p className="text-white" > { placeRdx.find((p) => p.id === r.placeId)?.name  }</p>
                         </div>
-                                     ))}
-        </div>
+                        <div className="">
+                            <p className={Styles.dataText}>from</p>
+                            <p className="text-white">  {r.dateInit.slice(0, 10)}</p>
+                        </div>
+                        <div className="">
+                            <p className={Styles.dataText}>to</p>
+                            <p className="text-white"> {r.dateEnd.slice(0, 10)}</p>
+                        </div>
+                        <div className="">
+                            <p className={Styles.dataText}>price</p>
+                            <p className="text-white"> {r.total }$</p>
+                        </div>
+                        <div className="">
+                            <p className={Styles.dataText}>status</p>
+                            <p className="text-white">  {r.status}</p>
+                        </div>
+                            <button className={Styles.btnPay}>Pay</button>
+                            <button className={Styles.btnCancel}>Cancel</button>
+                   
+                    
+                </div>
+
+             ))}
+    </div>
         )}
     </div>
  )
