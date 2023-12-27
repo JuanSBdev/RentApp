ad8232269f7fb326c3024d448da99de87649f1dc
Juan SB <juansb.dev@gmail.com>
2023-12-27 01:16:09 -0300

    ContacView FORM btn

--- client/src/views/Contact/ContactView.jsx
+++ client/src/views/Contact/ContactView.jsx
@@ -5,12 +5,17 @@ import {
     Button,
     Typography,
   } from "@material-tailwind/react";
+import { useSelector } from "react-redux";
    
-  export function ContactView() {
-    return (
-        <div className=" flex justify-center  p-4">
 
-       <Card color="white" shadow={false}>
+export function ContactView() {
+    
+      let lang = useSelector(state => state.language)
+
+    return (
+        <div className=" flex  justify-center p-4  max-h-24">
+
+       <Card className="p-4  " color="white" shadow={false}>
         <Typography variant="h4" color="blue-gray">
           Sign Up
         </Typography>
@@ -18,7 +23,7 @@ import {
           Nice to meet you! Enter your details to register.
         </Typography>
         <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-          <div className="mb-1 flex flex-col gap-6">
+          <div className="mb-1 flex flex-col gap-4">
             <Typography variant="h6" color="blue-gray" className="-mb-3">
               Your Name
             </Typography>
@@ -54,27 +59,17 @@ import {
             }}
             />
           </div>
-          <Checkbox
-            label={
-                <Typography
-                variant="small"
-                color="gray"
-                className="flex items-center font-normal"
-                >
-                I agree the
-                <a
-                  href="#"
-                  className="font-medium transition-colors hover:text-gray-900"
-                  >
-                  &nbsp;Terms and Conditions
-                </a>
-              </Typography>
-            }
-            containerProps={{ className: "-ml-2.5" }}
-            />
+          
+          {lang === 'es' ? (
           <Button className="mt-6" fullWidth>
-            sign up
+          Enviar
+        </Button>
+
+          ):(
+          <Button className="mt-6" fullWidth>
+            Send
           </Button>
+          )}
           <Typography color="gray" className="mt-4 text-center font-normal">
             Already have an account?{" "}
             <a href="#" className="font-medium text-gray-900">
