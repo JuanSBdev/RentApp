6424b46951f8cd0b66d976a9af6179b5bbc3ee03
Juan SB <juansb.dev@gmail.com>
2023-12-28 23:56:07 -0300

    Form start FIX vcp

--- client/src/components/FormStart/FormStart.jsx
+++ client/src/components/FormStart/FormStart.jsx
@@ -94,7 +94,7 @@ return (
              <label className="font-semibold text-xs" htmlFor="usernameField">City</label>
              <select onChange={handleForm} name='city' id="ciudades" className="flex items-center h-12 px-4 w-34  bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2">
                    <option value="capilla">Capilla del Monte</option>
-                   <option value="carlos">Carlos Paz</option>
+                   <option value="carlos">San Marcos Sierras</option>
                    <option value="falda">La Falda</option>
              </select>
              
