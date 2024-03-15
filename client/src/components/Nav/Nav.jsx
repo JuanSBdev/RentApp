ed8e5f817b0d2d16b97332cce52b3be762ac0841
Juan SB <juansb.dev@gmail.com>
2024-03-15 17:03:17 -0300

    fix nav left styles

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -32,7 +32,7 @@ export default function Nav() {
           <div className="navbar-start">
     <div className="dropdown">
       <label tabIndex={0} className="btn btn-ghost btn-circle">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" color='white' strokeLinejoin="round" strokeWidth="2" d="M3 6h16M5 12h16M4 18h7" /></svg>
       </label>
 
       {lang === 'es' ? ( <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
@@ -62,9 +62,9 @@ export default function Nav() {
     </div>
           </div>
           <div className=" flex items-end navbar-center">
-           <NavLink to={'/'} className=" normal-case text-xl text-[#fff]">Kirá
+           <NavLink to={'/'} className=" normal-case font-bold text-xl text-[#fff]">Kirá
           </NavLink>
-           <p className="text-[black] text-l ml-2"> Alquiler </p>
+           <p className="text-[white] text-xs ml-2 mb-1"> Argentina </p>
           </div>
         </div>
  
