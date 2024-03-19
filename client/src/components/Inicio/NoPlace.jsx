9c65c5456ad1cd8389d566664d511f7eb6be0eb0
Juan SB <juansb.dev@gmail.com>
2024-03-19 17:55:56 -0300

    tittle places FIX

--- client/src/components/Inicio/NoPlace.jsx
+++ client/src/components/Inicio/NoPlace.jsx
@@ -8,7 +8,10 @@ import Carlos from './CarlosPaz/SanMarcos';
 export default function NoPlace() {
   return (
     <div className="flex flex-col ">
-      <p>Explore our cities</p>
+      <div className="p-4 flex gap-1">
+      <div className={Styles.divider}></div>
+      <h2 className='text-white text-2xl'>Explore our cities</h2>
+      </div>
       
       <div className={`${Styles.wrapper} flex wrap  md:flex-row gap-6 xl:gap-4 max-w-screen-lg justify-around flex-wrap   items-stretch content-center` }>
       
