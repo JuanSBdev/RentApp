d8542efefd972f33846a61baf5989a38dc75bc92
Juan SB <juansb.dev@gmail.com>
2024-03-22 12:19:30 -0300

    back color

--- client/src/components/StartTitle/StartTitle.jsx
+++ client/src/components/StartTitle/StartTitle.jsx
@@ -8,9 +8,9 @@ export default function StartTitle() {
   return (
     <div className={`${Styles.wrapper} flex  justify-center md:justify-start w-100  ml-4  mt-10 mb-12 lg:mb-0  `}>
       {lang === 'es' ? (
-        <h1 className='flex justify-start '> Encontrá tu lugar en Córdoba, en Kirá</h1>
+        <h1 className='flex justify-start text-3xl  text-green-700  font-bold '> Encontrá tu lugar en Córdoba, en Kirá</h1>
       ):(
-        <h1 className='flex justify-start '> Discover your place in Córdoba, at Kirá</h1>
+        <h1 className='flex justify-start text-3xl  text-green-700  font-bold'> Discover your place in Córdoba, at Kirá</h1>
 
       )  }
     </div>
